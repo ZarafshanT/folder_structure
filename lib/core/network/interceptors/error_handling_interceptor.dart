0afa76e94da0783ccb54c9cb22b3ea8d348770d5
zarafshan-stepsharp <zarafshan@stepsharp.dev>
2026-06-24 20:06:44 +0500

    Update error_handling_interceptor.dart

--- lib/core/network/interceptors/error_handling_interceptor.dart
+++ lib/core/network/interceptors/error_handling_interceptor.dart
@@ -3,21 +3,35 @@ import 'package:dio/dio.dart';
 class ErrorHandlingInterceptor extends Interceptor {
   @override
   void onError(DioException err, ErrorInterceptorHandler handler) {
-    String errorMessage;
-    switch (err.type) {
-      case DioExceptionType.connectionTimeout:
-        errorMessage = "⏳ Connection timeout";
-        break;
-      case DioExceptionType.connectionError:
-        errorMessage = "📡 No Internet connection";
-        break;
-      case DioExceptionType.badResponse:
-        errorMessage = "💥 Server error: ${err.response?.statusCode}";
-        break;
-      default:
-        errorMessage = "⚠️ Unexpected error";
+    if (err.response != null) {
+      switch (err.response?.statusCode) {
+        case 400:
+          print("Bad Request");
+          break;
+        case 401:
+          print("Unauthorized");
+          break;
+        case 403:
+          print("Access Denied");
+          break;
+        case 500:
+          print("Server Error");
+          break;
+        default:
+          print("Server error: ${err.response?.statusCode}");
+      }
+    } else {
+      switch (err.type) {
+        case DioExceptionType.connectionTimeout:
+          print("⏳ Connection timeout");
+          break;
+        case DioExceptionType.connectionError:
+          print("📡 No Internet connection");
+          break;
+        default:
+          print("⚠️ Unexpected error: ${err.message}");
+      }
     }
-    print(errorMessage);
     return handler.next(err);
   }
 }
